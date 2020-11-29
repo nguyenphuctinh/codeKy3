490f28841bd5af6bf6fb41b747df07a03fa2835b
thochoikhon <73649278+thochoikhon@users.noreply.github.com>
2020-11-29 10:10:38 +0700

    Add files via upload

--- comination1.cpp
+++ comination1.cpp
@@ -1,30 +1,40 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int n,gt=0,a[123456];
-void Try(int dem)
+int n,ok=0;
+void Sinh(int a[])
 {
-	for(char j=0; j<2; j++)
-	{	
-		a[dem]=j;
-		if(dem!=n)	
-			Try(dem+1);
-		else
-		{
-		    for(int k=1; k<=dem; k++)
-		        cout<<a[k];
-		       cout<<" ";
-		}
+	int i=n;
+	while(a[i]!=0)
+		i--;
+	if(i==0)
+	{
+		ok=1;
+		return;
 	}
+	a[i]=1;
+	for(int j=i+1; j<=n; j++)
+		a[j]=0;	
 }
 int main()
 {
-	int t;cin>>t;	
+	int t;cin>>t;
 	while(t--)
-	{	
-		gt=0;
-		int dem=1;
+	{
 		cin>>n;
-		Try(dem);
+		int a[100]={0};
+		for(int i=1; i<=n; i++)
+				cout<<a[i];
+		cout<<" ";
+		ok=0;
+		while(true)
+		{
+			Sinh(a);
+			if(ok)
+				break;
+			for(int i=1; i<=n; i++)
+				cout<<a[i];
+			cout<<" ";
+		}
 		cout<<endl;
 	}
 }
